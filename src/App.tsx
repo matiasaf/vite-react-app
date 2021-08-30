c2c98eebfe9aa97fb087ca7d96414214948a4eb8
Matias Fernandez <mfernandez@techinsights.com>
2021-08-30 17:55:56 -0300

    modified some styles on the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,81 @@
+import axios from "axios";
 import React, { useState } from "react";
 import "./App.css";
-import HackersNews from "./HackersNews";
+import HackersNews, { IStory } from "./HackersNews";
+
+const URL = "https://hn.algolia.com/api/v1/search";
+interface APIResponse {
+  data: {
+    hits: IStory[];
+  };
+}
 
 function App() {
+  const [loading, setLoading] = useState(false);
   const [queryParam, setQueryParam] = useState("");
+  const [stories, setStories] = useState([] as IStory[]);
+  const [open, setOpen] = useState(false);
+  const [inStatus, setInStatus] = useState(false);
+  const [close, setClose] = useState(false);
+
+  function handleCloseInput() {
+    setQueryParam("");
+    setStories([]);
+    setClose(true);
+    setInStatus(false);
+    setOpen(false);
+
+    setTimeout(() => setClose(false), 1000);
+  }
+
+  function handleFocusStatus() {
+    if (open) return;
+    setClose(false);
+    setOpen(false);
+    setInStatus(true);
+    setTimeout(() => {
+      setOpen(true);
+      setInStatus(false);
+    }, 1000);
+  }
+
+  async function handleOnSearch(e: React.FormEvent<HTMLFormElement>) {
+    setLoading(true);
+    e.preventDefault();
+    const result = (await axios.get(
+      `${URL}?query=${queryParam}`
+    )) as APIResponse;
+    setStories(result.data.hits);
+    setLoading(false);
+  }
+
+  function handleChangeInput(e: any) {
+    if (!e.target.value) {
+      setStories([]);
+    }
+    setQueryParam(e.target.value);
+  }
 
   return (
     <div className="App">
-      <form>
-        <div className="form-group">
-          <h1 className="title">
-            Insert what do you need to search for in HackerNews
-          </h1>
-          <input
-            id="search-value"
-            value={queryParam}
-            onChange={(e) => setQueryParam(e.target.value)}
-          />
-        </div>
+      <h1 className="search-title">Search on HackersNews</h1>
+      <form
+        onSubmit={handleOnSearch}
+        className={`${inStatus ? "in" : ""} ${open ? "open" : ""} ${
+          close ? "close" : ""
+        }`}
+      >
+        <input
+          type="text"
+          onFocus={handleFocusStatus}
+          value={queryParam}
+          onChange={handleChangeInput}
+        />
+        <div className="after" onClick={handleCloseInput}></div>
+        <input type="submit" />
       </form>
       <div className="results">
-        <HackersNews query={queryParam} />
+        <HackersNews stories={stories} loading={loading}/>
       </div>
     </div>
   );
--- src/HackersNews.tsx
+++ src/HackersNews.tsx
@@ -1,57 +1,35 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
-
-const URL = "https://hn.algolia.com/api/v1/search";
-
-interface IStory {
+import "./HackersNews.css";
+export interface IStory {
   objectID: string;
   url: string;
   title: string;
 }
-
-interface APIResponse {
-  data: {
-    hits: IStory[];
-  };
-}
-
 interface IProps {
-  query: string;
+  stories: IStory[];
+  loading: boolean;
 }
 
 function HackersNews(props: IProps) {
-  const [stories, setStories] = useState([] as IStory[]);
-  const [error, setError] = useState("" as string);
+  const { stories } = props;
 
-  async function handleFetch() {
-    let result;
-    try {
-      result = (await axios.get(`${URL}?query=${props.query}`)) as APIResponse;
-      setStories(result.data.hits);
-    } catch (err: any) {
-      setError(err);
-    }
+  if (props.loading) {
+    return <div className="loading-container">Loading ...</div>;
+  } else {
+    return (
+      <div>
+        <ul className="stories-container">
+          {stories.map((story: IStory) => (
+            <li key={story.objectID} className="li-item">
+              <a href={story.url} className="link-story">
+                {story.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
   }
-
-  useEffect(() => {
-    if (props.query) {
-      handleFetch();
-    }
-  }, [props.query]);
-
-  return (
-    <div>
-      {error && <span>Something went wrong ...</span>}
-
-      <ul className="stories-container">
-        {stories.map((story: IStory) => (
-          <li key={story.objectID}>
-            <a href={story.url}>{story.title}</a>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
 }
 
 export default HackersNews;
